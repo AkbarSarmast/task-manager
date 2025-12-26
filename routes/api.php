5a85e1059a5c1e42602c59908b3f93b331818a9b
AkbarSarmast <aksa84ln@gmail.com>
2025-12-26 06:22:39 -0500

    add

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
@@ -12,10 +11,12 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::apiResource('tasks', TaskController::class)->middleware('auth:sanctum');
-Route::apiResource('categories', CategoryController::class)->middleware('auth:sanctum');
-Route::apiResource('tasks.comments', CommentController::class)->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('tasks.comments', CommentController::class);
+});
